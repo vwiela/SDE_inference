f6d66145de2cd5108dbff030a7f5efa492bc6d01
Vincent Wieland <90847066+vwiela@users.noreply.github.com>
2023-05-03 11:00:32 +0200

    Make it a module for nicer integration as a local package

--- src/SDE_inference.jl
+++ src/SDE_inference.jl
@@ -1,3 +1,6 @@
+module SDE_inference
+
+using CSV
 using Distributions 
 using Random 
 using LinearAlgebra 
@@ -7,7 +10,27 @@ using StaticArrays
 using DataFrames
 using Printf
 using ProgressMeter
-using Printf
+using DelimitedFiles
+
+export init_sde_model
+export DynModInput
+export simulate_data_sde
+export SimResult
+export write_simulated_data
+export init_param
+export init_filter
+export init_file_loc
+export init_mcmc
+export RamSampler
+export init_pilot_run
+export tune_particles_single_individual
+export change_start_val_to_pilots
+export init_mcmc_pilot
+export init_filter
+export BootstrapEm
+export run_mcmc
+
+
 
 include(joinpath(@__DIR__, "Structs.jl"))
 include(joinpath(@__DIR__, "Common.jl"))
@@ -17,3 +40,5 @@ include(joinpath(@__DIR__, "SDE.jl"))
 include(joinpath(@__DIR__, "Single_individual_inference.jl"))
 include(joinpath(@__DIR__, "Single_individual_pilot.jl"))
 include(joinpath(@__DIR__, "Bootstrap_filter.jl"))
+
+end
